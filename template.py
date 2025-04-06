a9294336c73f606b9ab71d4daaa4170e4483f667
entbappy <entbappy73@gmail.com>
2025-04-06 14:04:41 +0600

    setup added

--- template.py
+++ template.py
@@ -28,6 +28,7 @@ list_of_files = [
     f"{project_name}/pipeline/training_pipeline.py",
     f"{project_name}/utils/__init__.py",
     f"{project_name}/utils/util.py",
+    "config/config.yaml",
     ".dockerignore",
     "app.py",
     "Dockerfile",
